49ba94a3b3070194446262bf5c87738fa19e27a7
michellekhanan <116992090+michellekhanan@users.noreply.github.com>
2024-01-02 22:42:38 -0500

    isAnagram answer

---
+++ assignments/is_anagram.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        ssorted = sorted(s)
+        tsorted = sorted(t)
+        return ssorted == tsorted
